5be27cd1ac5fc41eaf989de29134e0966034fc6b
huxh7500 <86956846+huxh7500@users.noreply.github.com>
2021-10-09 16:13:03 +0300

    new update

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDKvxujLe8QZivQW97Z_OGF23xMsCnadZE",
+    authDomain: "whatsapp-yossi.firebaseapp.com",
+    projectId: "whatsapp-yossi",
+    storageBucket: "whatsapp-yossi.appspot.com",
+    messagingSenderId: "430436735760",
+    appId: "1:430436735760:web:9559d2061702a1c5916395"
+  };
