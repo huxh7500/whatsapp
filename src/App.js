10f8b263e0db1f9e60a14c0e8449cb94553ba580
huxh7500 <86956846+huxh7500@users.noreply.github.com>
2021-10-09 16:21:01 +0300

    new update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
       <h1>Whatsapp clone</h1>
+      <Sidebar/>
     </div>
   );
 }
